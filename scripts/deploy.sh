57e59f6353d3cc16c2dc72481dabaac796048509
fallax <fallaburr@gmail.com>
2017-04-27 21:40:01 +0000

    Added setup information to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Before running this script you must have installed the AWS CLI.
+# On ubuntu, do:
+# 1) sudo apt-get install python-pip python-dev build-essential 
+# 2) pip install --upgrade --user awscli
+# 3) Set up the AWS credentials using aws configure
+
 bundle exec jekyll build --config _config-live.yml
 aws s3 sync ./_site/ s3://genderkit.org.uk/
