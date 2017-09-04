bd8fa9968cb223b7ac90cc824a2363b50c147498
fallax <fallaburr@gmail.com>
2017-09-04 11:17:38 +0000

    Scripts: request logs now concatenated before analysis to make analysis report easier to read

--- scripts/log-analysis.sh
+++ scripts/log-analysis.sh
@@ -5,6 +5,17 @@
 # 1) sudo apt-get install python-pip python-dev build-essential 
 # 2) pip install --upgrade --user awscli
 # 3) Set up the AWS credentials using aws configure
+#
+# You will also need to install request-log-analyzer:
+# gem install request-log-analyzer
 
+echo "Downloading request log files..."
 aws s3 sync s3://genderkit/logs ./logs/logfiles/
-request-log-analyzer --no-progress --format amazon_s3 ./logs/logfiles/* --output html --file ./logs/log-analysis.html
+
+echo "Concatenating logs..."
+cat ./logs/logfiles/* > ./logs/concatenated.txt
+
+echo "Generating report..."
+request-log-analyzer --no-progress --format amazon_s3 ./logs/concatenated.txt --output html --file ./logs/log-analysis.html
+
+echo "Done."
