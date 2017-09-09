2ad7865886396aa33c2c8a0a9174cfd74ece7e1a
fallax <fallaburr@gmail.com>
2017-09-09 13:32:53 +0000

    Scripts: added broken link checker

--- scripts/broken-link-checker.sh
+++ scripts/broken-link-checker.sh
@@ -2,6 +2,9 @@
 
 cd ~
 npm install broken-link-checker
+echo "Searching for broken links. This may take several minutes."
 blc https://genderkit.github.io/genderkit -rf > broken-link-checker-results.txt
-echo "Results written to ~/broken-link-checker-results.txt."
+mv ~/broken-link-checker-results.txt /vagrant/
+echo "Results written to /vagrant/broken-link-checker-results.txt"
+cat /vagrant/broken-link-checker-results.txt | grep BROKEN
 echo "Done."
