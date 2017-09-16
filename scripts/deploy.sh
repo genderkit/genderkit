7d9390852749f8704614e5111375cea1bc46ccfa
fallax <fallaburr@gmail.com>
2017-09-16 20:34:15 +0000

    Scripts: deploy script now checks out pristine copy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,12 @@
 # 2) pip install --upgrade --user awscli
 # 3) Set up the AWS credentials using aws configure
 
+cd /vagrant
+mkdir live
+cd live
+git clone git@github.com:genderkit/genderkit.git
+cd genderkit
+bundle install
 bundle exec jekyll build --config _config-live.yml
 aws s3 sync ./_site/ s3://genderkit.org.uk/
+rm -rv /vagrant/live
