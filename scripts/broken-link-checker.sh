058818f7bd45396788c8fe21bc9859371ed5c754
fallax <fallaburr@gmail.com>
2017-11-09 23:23:12 +0000

    Scripts: updated to use live site URL

--- scripts/broken-link-checker.sh
+++ scripts/broken-link-checker.sh
@@ -3,7 +3,7 @@
 cd ~
 npm install broken-link-checker
 echo "Searching for broken links. This may take several minutes."
-blc https://genderkit.github.io/genderkit -rf > broken-link-checker-results.txt
+blc http://genderkit.org.uk -rf > broken-link-checker-results.txt
 mv ~/broken-link-checker-results.txt /vagrant/
 echo "Results written to /vagrant/broken-link-checker-results.txt"
 cat /vagrant/broken-link-checker-results.txt | grep BROKEN
