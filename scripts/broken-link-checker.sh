7fbc81fd01bc86ab16fe6c47afc91eaf9af20a4e
fallax <fallaburr@gmail.com>
2018-03-29 19:45:48 +0000

    Fixed #78: broken link checker now checks more links

--- scripts/broken-link-checker.sh
+++ scripts/broken-link-checker.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 cd ~
+mkdir broken-link-checker-results
 npm install broken-link-checker
 echo "Searching for broken links. This may take several minutes."
-blc http://genderkit.org.uk -rf > broken-link-checker-results.txt
-mv ~/broken-link-checker-results.txt /vagrant/
-echo "Results written to /vagrant/broken-link-checker-results.txt"
-cat /vagrant/broken-link-checker-results.txt | grep BROKEN
+blc http://genderkit.github.io/genderkit/error/ -rfv --exclude "organisations" --exclude "publications" > broken-link-checker-results/main.txt
+blc http://genderkit.github.io/genderkit/organisations/ -rfv > broken-link-checker-results/organisations.txt
+blc http://genderkit.github.io/genderkit/publications/ -rfv > broken-link-checker-results/publications.txt
+echo "Results written to ~/broken-link-checker-results/"
+cat ~/broken-link-checker-results/* | grep BROKEN
 echo "Done."
