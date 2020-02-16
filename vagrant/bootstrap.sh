95d671283199ce1a1c87c0ed24020e816ccaa286
fallax <>
2020-02-16 12:35:49 +0000

    Vagrant: major changes to bootstrap script

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,25 +1,9 @@
 sudo apt-get update
-sudo apt-get install -y git ruby ruby-dev make gcc dos2unix nodejs npm libcurl4-openssl-dev aspell aspell-en software-properties-common
-ln -s /usr/bin/nodejs /usr/bin/node
-
-sudo apt-get install linux-headers-$(uname -r) build-essential dkms
-wget http://download.virtualbox.org/virtualbox/6.1.2/VBoxGuestAdditions_6.1.2.iso
-sudo mkdir /media/VBoxGuestAdditions
-sudo mount -o loop,ro VBoxGuestAdditions_6.1.2.iso /media/VBoxGuestAdditions
-sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
-rm VBoxGuestAdditions_6.1.2.iso
-sudo umount /media/VBoxGuestAdditions
-sudo rmdir /media/VBoxGuestAdditions
-
+sudo apt-get install -y git ruby ruby-dev make gcc dos2unix nodejs npm libcurl4-openssl-dev aspell aspell-en software-properties-common zlib1g-dev build-essential patch zlib1g-dev liblzma-dev libxslt1-dev imagemagick linux-headers-$(uname -r) build-essential dkms
 gem install jekyll bundler rake
-
-if [ ! -d "/vagrant/git" ]; then
-  mkdir /vagrant/git
-fi
+sudo mount -t vboxsf -o uid=$UID,gid=$(id -g) vagrant /vagrant
+mkdir /vagrant/git
 cd /vagrant/git
-if [ ! -d "/vagrant/git/genderkit" ]; then
-  git clone git@github.com:genderkit/genderkit.git
-fi
+git clone git@github.com:genderkit/genderkit.git
 cd /vagrant/git/genderkit
-
 bundle install
