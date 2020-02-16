8c24e84026c9b88668dda2cf1c9d98aeab6bf8c1
fallax <>
2020-02-16 12:34:12 +0000

    Scripts: thumbnailing script now easier to run

--- scripts/thumbnail-publications.sh
+++ scripts/thumbnail-publications.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+cp /vagrant/git/genderkit/scripts/archive-publications.js ~
+cd ~
+npm install yamljs slug date-format
+nodejs ./archive-publications.js ~/archived-publications/
+cd ~/archived-publications
+cp /vagrant/git/genderkit/scripts/thumbnail-folder.sh .
 for f in *.pdf; do convert -thumbnail 100x141 -quality 75 -extent 100x141 -gravity center -background white -alpha remove "$f"[0] "/vagrant/git/genderkit/assets/images/publications/${f%.pdf}.jpg"; done
 cp "/vagrant/git/genderkit/assets/images/Gendered Intelligence - Trans Youth Sexual Health Booklet.jpg" "/vagrant/git/genderkit/assets/images/publications/"
