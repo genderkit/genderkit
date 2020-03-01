802becb2b3be0fa078c69a803dba86937a9b580e
fallax <>
2020-03-01 16:54:32 +0000

    Scripts: fixed old code mistaken left in

--- scripts/thumbnail-publications.sh
+++ scripts/thumbnail-publications.sh
@@ -5,6 +5,5 @@ cd ~
 npm install yamljs slug date-format
 nodejs ./archive-publications.js ~/archived-publications/
 cd ~/archived-publications
-cp /vagrant/git/genderkit/scripts/thumbnail-folder.sh .
 for f in *.pdf; do convert -thumbnail 100x141 -quality 75 -extent 100x141 -gravity center -background white -alpha remove "$f"[0] "/vagrant/git/genderkit/assets/images/publications/${f%.pdf}.jpg"; done
 cp "/vagrant/git/genderkit/assets/images/Gendered Intelligence - Trans Youth Sexual Health Booklet.jpg" "/vagrant/git/genderkit/assets/images/publications/"
