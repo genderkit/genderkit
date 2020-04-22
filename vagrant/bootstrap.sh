c2468ab4b5c616f6d378a49414564f97705559bf
fallax <2036067+fallax@users.noreply.github.com>
2020-04-22 18:17:54 +0000

    Vagrant: workaround for ubuntu VM problem where machine booted so slowly that shared folder would not be set up correctly

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,9 +1,7 @@
+#!/bin/sh
+echo "Running bootstrap script"
 sudo apt-get update
 sudo apt-get install -y git ruby ruby-dev make gcc dos2unix nodejs npm libcurl4-openssl-dev aspell aspell-en software-properties-common zlib1g-dev build-essential patch zlib1g-dev liblzma-dev libxslt1-dev imagemagick linux-headers-$(uname -r) build-essential dkms
 gem install jekyll bundler rake
-sudo mount -t vboxsf -o uid=$UID,gid=$(id -g) vagrant /vagrant
-mkdir /vagrant/git
-cd /vagrant/git
-git clone git@github.com:genderkit/genderkit.git
-cd /vagrant/git/genderkit
-bundle install
+echo "Bootstrap script done."
+echo "Virtual machine should now be set up - run 'vagrant ssh' to connect to it."
